4b4ae810c1eddd67aaa77bd6ac0caf6c97fe758d
Karl <karlding@users.noreply.github.com>
2017-08-26 01:01:10 -0700

    ELEC-246: Add golang packages

--- chef/uwmidsun-cookbooks/stm32-dev/recipes/default.rb
+++ chef/uwmidsun-cookbooks/stm32-dev/recipes/default.rb
@@ -44,3 +44,25 @@ bash 'install_ruby' do
     rvm install ruby-2.3.3
   EOH
 end
+
+apt_repository 'glide' do
+  uri 'ppa:masterminds/glide'
+  distribution node['lsb']['codename']
+  components ['main']
+  keyserver 'keyserver.ubuntu.com'
+  key '890C81B2'
+  deb_src true
+  action :add
+end
+
+ruby_block "insert_line" do
+  block do
+    file = Chef::Util::FileEdit.new("/home/vagrant/.bashrc")
+    file.insert_line_if_no_match("/export GOPATH=\/home\/vagrant\/go/", "export GOPATH=/home/vagrant/go")
+    file.insert_line_if_no_match("/export GOBIN=$GOPATH\/bin/", "export GOBIN=$GOPATH/bin")
+    file.insert_line_if_no_match("/export PATH=$GOPATH\/bin:$PATH/", "export PATH=$GOPATH/bin:$PATH")
+    file.write_file
+  end
+end
+
+package 'glide'
