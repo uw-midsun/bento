7fd0681bca5c54395a19b53ce79467d3e73bf1a4
karl ding <karlding@users.noreply.github.com>
2019-04-28 20:21:11 -0400

    ELEC-619: Update 18.04 bento recipes and ISO URL (#13)
    Update Ubuntu bento scripts with updated versions of the cleanup,
    networking setup and package update scripts, as well as new URL
    for the 18.04 LTS server image.

--- scripts/ubuntu/cleanup.sh
+++ scripts/ubuntu/cleanup.sh
@@ -42,6 +42,19 @@ apt-get -y purge ppp pppconfig pppoeconf;
 # Delete oddities
 apt-get -y purge popularity-contest installation-report command-not-found command-not-found-data friendly-recovery;
 
+# Exlude the files we don't need w/o uninstalling linux-firmware
+echo "==> Setup dpkg excludes for linux-firmware"
+cat <<_EOF_ | cat >> /etc/dpkg/dpkg.cfg.d/excludes
+#BENTO-BEGIN
+path-exclude=/lib/firmware/*
+path-exclude=/usr/share/doc/linux-firmware/*
+#BENTO-END
+_EOF_
+
+# Delete the massive firmware packages
+rm -rf /lib/firmware/*
+rm -rf /usr/share/doc/linux-firmware/*
+
 apt-get -y autoremove;
 apt-get -y clean;
 
@@ -55,3 +68,6 @@ find /var/cache -type f -exec rm -rf {} \;
 
 # delete any logs that have built up during the install
 find /var/log/ -name *.log -exec rm -f {} \;
+
+# Blank netplan machine-id (DUID) so machines get unique ID generated on boot.
+truncate -s 0 /etc/machine-id
--- scripts/ubuntu/update.sh
+++ scripts/ubuntu/update.sh
@@ -1,39 +1,38 @@
 #!/bin/sh -eux
+export DEBIAN_FRONTEND=noninteractive
 
 ubuntu_version="`lsb_release -r | awk '{print $2}'`";
-ubuntu_major_version="`echo $ubuntu_version | awk -F. '{print $1}'`";
-
-# Work around bad cached lists on Ubuntu 12.04
-if [ "$ubuntu_version" = "12.04" ]; then
-    apt-get clean;
-    rm -rf /var/lib/apt/lists;
-fi
+major_version="`echo $ubuntu_version | awk -F. '{print $1}'`";
 
 # Disable release-upgrades
 sed -i.bak 's/^Prompt=.*$/Prompt=never/' /etc/update-manager/release-upgrades;
 
+# Disable systemd apt timers/services
+if [ "$major_version" -ge "16" ]; then
+  systemctl stop apt-daily.timer;
+  systemctl stop apt-daily-upgrade.timer;
+  systemctl disable apt-daily.timer;
+  systemctl disable apt-daily-upgrade.timer;
+  systemctl mask apt-daily.service;
+  systemctl mask apt-daily-upgrade.service;
+  systemctl daemon-reload;
+fi
+
+# Disable periodic activities of apt to be safe
+cat <<EOF >/etc/apt/apt.conf.d/10periodic;
+APT::Periodic::Enable "0";
+APT::Periodic::Update-Package-Lists "0";
+APT::Periodic::Download-Upgradeable-Packages "0";
+APT::Periodic::AutocleanInterval "0";
+APT::Periodic::Unattended-Upgrade "0";
+EOF
+
+# Clean and nuke the package from orbit
+rm -rf /var/log/unattended-upgrades;
+apt-get -y purge unattended-upgrades;
+
 # Update the package list
 apt-get -y update;
 
-# update package index on boot
-cat <<EOF >/etc/init/refresh-apt.conf;
-description "update package index"
-start on networking
-task
-exec /usr/bin/apt-get update
-EOF
-
-# Manage broken indexes on distro disc 12.04.5
-if [ "$ubuntu_version" = "12.04" ]; then
-    apt-get -y install libreadline-dev dpkg;
-fi
-
-# Disable periodic activities of apt
-cat <<EOF >/etc/apt/apt.conf.d/10disable-periodic;
-APT::Periodic::Enable "0";
-EOF
-
 # Upgrade all installed packages incl. kernel and kernel headers
-apt-get -y dist-upgrade;
-reboot;
-sleep 60;
+apt-get -y dist-upgrade -o Dpkg::Options::="--force-confnew";
